0095ea19472d5457733b17e01e28fde239e2890e
inspirit941 <inspirit941@gmail.com>
2022-05-28 11:37:23 +0900

    fix: CORS issue

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -15,5 +15,21 @@ func InitRouter(server *gin.Engine) *gin.Engine {
 		v1.POST("/upload", api.ConvertFile)
 	}
 	server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	server.Use(CORSMiddleware())
 	return server
+}
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
