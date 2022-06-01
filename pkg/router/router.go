1ba0ab8ec42bc4fbff7a21976956c21895d588fe
Donggeon Lee <inspirit941@gmail.com>
2022-06-02 05:50:54 +0900

    Update router.go

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -20,7 +20,7 @@ func InitRouter(server *gin.Engine) *gin.Engine {
 }
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080, https://delightful-field-0835ff900.1.azurestaticapps.net, http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
